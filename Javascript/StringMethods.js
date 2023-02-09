602eb90460d70ee45fb9ddd817640d20ab6b54a2
SandipDeb <sandipdeb05@gmail.com>
2023-02-09 20:59:12 +0530

    completed learning string methods

--- Javascript/StringMethods.js
+++ Javascript/StringMethods.js
@@ -70,3 +70,55 @@ console.log(str15.padEnd(10, "*"));
 // TODO str.codePointAt(pos) -> The codePointAt() method returns an integer that denotes the Unicode point value of a character in the string.
 const str16 = "sandip";
 console.log(str16.codePointAt(0)); // s -> 115
+
+// TODO String.fromCodePoint(num1, ..., numN) -> The fromCodePoint() method returns a string created by using the given sequence of unicode code points.
+const str17 = String.fromCharCode(65, 68, 70, 72);
+console.log(str17);
+
+// TODO str.match(regexp) -> the match() method returns the result of matching a string against a regular expression.
+const str18 = "JavaScript is a fun programming language";
+console.log(str18.match("programming"));
+
+// TODO str.matchAll(regexp) -> The matchAll() method returns an iterator of results after matching a string against a regular expression.
+const str19 = "JavaScript1JavaScript2";
+const regex = /JavaScript\d/g;
+const res19 = str19.matchAll(regex);
+for (let r of res19) {
+  console.log(r);
+}
+
+// TODO str.localeCompare(compareStr, locales, options) -> The localeCompare() method checks if a given string comes before, after, or is equivalent as another string in sort order.
+const res20 = "a".localeCompare("b"); // -1
+const res21 = "b".localeCompare("a"); // 1
+const res22 = "a".localeCompare("z"); // -1
+const res23 = "a".localeCompare("a"); // 0
+const res24 = "book".localeCompare("cook"); // -1
+console.log(res20, res21, res22, res23, res24);
+
+// TODO str.search(regexp) -> The search() method searches for a match between a given string and a regular expression.
+const str20 = "I love to code in JavaScript.";
+console.log(str20.search("code"));
+
+// TODO str.replaceAll(pattern, replacement) -> The replaceAll() method returns a new string with all matches of a pattern replaced by a replacement.
+const str21 = "bbabb ccacc";
+console.log(str21.replaceAll("b", "c")); // ccacc ccacc
+console.log(str21.replace("b", "c")); // cbabb ccacc
+
+// TODO str.concat(str1, ..., strN) -> The concat() method concatenates given arguments to the given string.
+const str22 = "the";
+const str23 = " javascript";
+const str24 = str22.concat(str23, " is cool");
+console.log(str24);
+
+// TODO str.split(separator, limit) -> The split() method divides a string into a list of substrings and returns them as an array.
+const str25 = "JavaScript:React:Node : mongodb";
+console.log(str25.split(":"));
+console.log(str25.split(":", 2));
+
+// TODO str.trim() -> The trim() method removes whitespace from both ends of a string.
+const str26 = "   plane simple    text    ";
+console.log(str26.trim());
+
+// TODO str.slice(beginIndex, endIndex) -> The slice() method extracts and returns a section of a string.
+const str27 = "JavaScript is fun";
+console.log(str27.slice(0, 10));
