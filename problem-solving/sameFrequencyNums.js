a9036a4d85fb41c22825e94d25e9a84920bf2c76
SandipDeb <sandipdeb05@gmail.com>
2023-07-13 10:06:03 +0530

    solve same frequency problem

--- problem-solving/sameFrequencyNums.js
+++ problem-solving/sameFrequencyNums.js
@@ -1,30 +1,27 @@
 function sameFrequencyNums(num1, num2) {
-  const num1Freq = getFrequency(num1);
-  const num2Freq = getFrequency(num2);
+  const num1Str = num1.toString();
+  const num2Str = num2.toString();
 
-  for (let key in num1Freq) {
-    if (!(key in num2Freq)) return false;
-    if (num1Freq[key] !== num2Freq[key]) return false;
+  if (num1Str.length !== num2Str.length) return false;
+
+  const freq1 = {};
+  const freq2 = {};
+
+  for (let i = 0; i < num1Str.length; i++) {
+    freq1[num1Str[i]] = ++freq1[num1Str[i]] || 1;
+    freq2[num2Str[i]] = ++freq2[num2Str[i]] || 1;
+  }
+
+  for (let key in freq1) {
+    if (!(key in freq2)) return false;
+    if (freq1[key] !== freq2[key]) return false;
   }
 
   return true;
 }
 
-function getFrequency(num) {
-  const freq = {};
-
-  const numArray = String(num)
-    .split("")
-    .map((item) => Number(item));
-
-  for (let item of numArray) {
-    freq[item] = (freq[item] || 0) + 1;
-  }
-  return freq;
-}
-
-// console.log(sameFrequencyNums(182, 281)); // true
-// console.log(sameFrequencyNums(34, 14)); // false
-// console.log(sameFrequencyNums(3589578, 5879385)); // true
-// console.log(sameFrequencyNums(22, 222)); // false
+console.log(sameFrequencyNums(182, 281)); // true
+console.log(sameFrequencyNums(34, 14)); // false
+console.log(sameFrequencyNums(3589578, 5879385)); // true
+console.log(sameFrequencyNums(22, 222)); // false
 console.log(sameFrequencyNums(987654, 456789)); // true
