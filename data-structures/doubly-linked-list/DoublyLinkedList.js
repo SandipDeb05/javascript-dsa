01a100ad88a098ed2864727dc83facd442a68933
SandipDeb <sandipdeb05@gmail.com>
2023-08-17 22:05:07 +0530

    learned doubly linkedlist

--- data-structures/doubly-linked-list/DoublyLinkedList.js
+++ data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -1,8 +1,8 @@
 class Node {
-  constructor(val) {
-    this.value = val;
-    this.next = null;
+  constructor(value) {
+    this.value = value;
     this.prev = null;
+    this.next = null;
   }
 }
 
@@ -12,6 +12,7 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  // O(1)
   push(val) {
     const newNode = new Node(val);
     if (!this.head) {
@@ -25,7 +26,9 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  // O(1)
   pop() {
+    if (!this.head) return undefined;
     const currTail = this.tail;
     if (this.length === 1) {
       this.head = null;
@@ -38,7 +41,9 @@ class DoublyLinkedList {
     this.length--;
     return currTail;
   }
+  // O(1)
   shift() {
+    if (this.length === 0) return null;
     const currHead = this.head;
     if (this.length === 1) {
       this.head = null;
@@ -51,7 +56,9 @@ class DoublyLinkedList {
     this.length--;
     return currHead;
   }
+  // O(1)
   unshift(val) {
+    if (!val) return;
     const newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
@@ -64,27 +71,30 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  // O(n)
   get(index) {
-    if (index < 0 || index >= this.length) return false;
-    let mid = Math.floor(this.length / 2);
+    if (index < 0 || index >= this.length) return null;
+    const mid = Math.floor(this.length / 2);
+    let node;
     if (index <= mid) {
-      let currHead = this.head;
       let counter = 0;
+      node = this.head;
       while (counter !== index) {
-        currHead = currHead.next;
+        node = node.next;
         counter++;
       }
-      return currHead;
     } else {
-      let currTail = this.tail;
       let counter = this.length - 1;
+      node = this.tail;
       while (counter !== index) {
-        currTail = currTail.prev;
+        console.log(counter);
+        node = node.prev;
         counter--;
       }
-      return currTail;
     }
+    return node;
   }
+  // O(n)
   set(index, val) {
     const node = this.get(index);
     if (node) {
@@ -93,17 +103,71 @@ class DoublyLinkedList {
     }
     return false;
   }
+  // O(n)
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+
+    const newNode = new Node(val);
+    const prevNode = this.get(index - 1);
+    const nextNode = prevNode.next;
+
+    prevNode.next = newNode;
+    newNode.prev = prevNode;
+    newNode.next = nextNode;
+    nextNode.prev = newNode;
+    this.length++;
+    return true;
+  }
+  // O(n)
+  remove(index) {
+    if (index < 0 || index >= this.length) return false;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    const node = this.get(index);
+    const prevNode = node.prev;
+    const nextNode = node.next;
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+
+    node.next = null;
+    node.prev = null;
+    this.length--;
+    return node;
+  }
 }
 
 const dll = new DoublyLinkedList();
-
 dll.push(10);
 dll.push(20);
 dll.push(30);
-// dll.pop();
-// dll.pop();
-// dll.shift();
-dll.unshift(100);
+dll.push(40);
+dll.push(50);
+dll.push(60);
+dll.push(70);
 
-console.log(dll.set(2, 1000));
+// dll.pop();
+// dll.pop();
+
+// dll.shift();
+// dll.shift();
+// dll.shift();
+
+// dll.unshift(5);
+
+// console.log(dll.get(5));
+// dll.set(5, 100);
+
+// dll.insert(0, 200);
+
+dll.remove(2);
+
 console.log(dll);
+
+// const first = new Node(10);
+// first.next = new Node(20);
+// first.next.prev = first;
+
+// console.log(first);
