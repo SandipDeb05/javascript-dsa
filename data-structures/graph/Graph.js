87b11310faafa1c6226ce1842d092e28f5bdfee0
SandipDeb <sandipdeb05@gmail.com>
2023-08-21 21:01:23 +0530

    learned graph

---
+++ data-structures/graph/Graph.js
@@ -0,0 +1,49 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
+
+const g = new Graph();
+
+g.addVertex("Tokyo");
+g.addVertex("Newyork");
+g.addVertex("California");
+g.addVertex("India");
+
+g.addEdge("Tokyo", "California");
+g.addEdge("Tokyo", "Newyork");
+
+g.addEdge("India", "Newyork");
+
+// g.removeEdge("Tokyo", "Newyork");
+// g.removeEdge("Tokyo", "California");
+
+g.removeVertex("Tokyo");
+
+console.log(g);
