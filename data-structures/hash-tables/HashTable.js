ac3f7cc7b1e069beb81f22bf376426c9c595c288
SandipDeb <sandipdeb05@gmail.com>
2023-08-21 14:07:49 +0530

    learned hashtable

--- data-structures/hash-tables/HashTable.js
+++ data-structures/hash-tables/HashTable.js
@@ -1,10 +1,75 @@
-function hash(key, arrayLen) {
-  let total = 0;
-  let WEIRD_PRIME = 31;
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i];
-    let value = char.charCodeAt(0) - 96;
-    total = (total + WEIRD_PRIME + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total + WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  // O(1)
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+    return index;
+  }
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
 }
+
+const ht = new HashTable(15);
+
+ht.set("black", "#000000");
+ht.set("white", "#ffffff");
+ht.set("gray", "#c4c4c4c4");
+ht.set("pink", "#ff00dd");
+ht.set("yellow", "#ffff00");
+ht.set("pink", "#ff00dd");
+
+console.log(ht.keys());
+console.log(ht.values());
+
+console.log(ht);
