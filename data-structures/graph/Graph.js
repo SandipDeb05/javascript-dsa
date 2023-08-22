9f7efe65da9e437ce331031031b8ac162a15ce11
SandipDeb <sandipdeb05@gmail.com>
2023-08-22 22:09:31 +0530

    learned dfs and bfs in graph

--- data-structures/graph/Graph.js
+++ data-structures/graph/Graph.js
@@ -27,23 +27,76 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  dfsRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+    return result;
+  }
+  dfsIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    visited[start] = true;
+    let currVertex;
+    while (stack.length) {
+      currVertex = stack.pop();
+      result.push(currVertex);
+      this.adjacencyList[currVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  bfs(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    visited[start] = true;
+    let currVertex;
+    while (queue.length) {
+      currVertex = queue.shift();
+      result.push(currVertex);
+      this.adjacencyList[currVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 const g = new Graph();
 
-g.addVertex("Tokyo");
-g.addVertex("Newyork");
-g.addVertex("California");
-g.addVertex("India");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
 
-g.addEdge("Tokyo", "California");
-g.addEdge("Tokyo", "Newyork");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
 
-g.addEdge("India", "Newyork");
-
-// g.removeEdge("Tokyo", "Newyork");
-// g.removeEdge("Tokyo", "California");
-
-g.removeVertex("Tokyo");
-
-console.log(g);
+console.log(g.bfs("A"));
